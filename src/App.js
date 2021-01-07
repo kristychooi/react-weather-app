81d70b166303e52541771357aaffb28bca981c00
kyychooi <kristychooi@gmail.com>
2021-01-07 12:41:18 -0800

    Fixed security concern
    https://html.spec.whatwg.org/multipage/links.html#link-type-noopener

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ export default function App() {
           <a
             href="https://github.com/kristychooi/react-weather-app"
             target="_blank"
+            rel="noreferrer"
           >
             Open-source code
           </a>{" "}
