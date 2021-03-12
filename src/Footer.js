d486f59e96f85f815cbc2dabf9de27bb48022b3f
kyychooi <kristychooi@gmail.com>
2021-03-11 18:34:20 -0800

    CSS updated

--- src/Footer.js
+++ src/Footer.js
@@ -3,15 +3,37 @@ import "./Footer.css";
 
 export default function Footer() {
   return (
-    <div className="footer">
-      <a
-        href="https://github.com/kristychooi/react-weather-app"
-        target="_blank"
-        rel="noreferrer"
-      >
-        Open-source code
-      </a>{" "}
-      by Kristy Chooi
+    <div>
+      <div className="footer">
+        <a
+          href="https://github.com/kristychooi/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Open-source code
+        </a>{" "}
+        by Kristy Chooi
+      </div>
+      <div className="attribution">
+        Icons made by{" "}
+        <a
+          href="https://www.flaticon.com/authors/dinosoftlabs"
+          title="DinosoftLabs"
+        >
+          DinosoftLabs{" "}
+        </a>
+        and{" "}
+        <a
+          href="https://www.flaticon.com/authors/nikita-golubev"
+          title="Nikita Golubev"
+        >
+          Nikita Golubev
+        </a>{" "}
+        from{" "}
+        <a href="https://www.flaticon.com/" title="Flaticon">
+          www.flaticon.com
+        </a>
+      </div>
     </div>
   );
 }
